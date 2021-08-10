f3e433a28698c2f1cb1a2a68296061dfde024c03
Ziv Kaplan <zivkaplan@gmail.com>
2021-08-11 00:55:14 +0300

    first react router commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import seedColors from './seedColors';
 import Palette from './components/Palette';
 import generatePalette from './colorHelpers';
 class App extends React.Component {
+    findPalette(id) {
+        return seedColors.find((palette) => palette.id === id);
+    }
     render() {
         return (
-            <div className="App">
-                <Palette palette={generatePalette(seedColors[4])} />
-            </div>
+            <Switch>
+                <Route exact path="/" render={() => <h1>h1</h1>} />
+                <Route
+                    exact
+                    path="/palette/:id"
+                    render={(routeProps) => (
+                        <Palette
+                            palette={generatePalette(
+                                this.findPalette(routeProps.match.params.id)
+                            )}
+                        />
+                    )}
+                />
+            </Switch>
+            // <div className="App">
+            //     <Palette palette={generatePalette(seedColors[4])} />
+            // </div>
         );
     }
 }
